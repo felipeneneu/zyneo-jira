0f4d05e956150c1a5b6c0512d339d02ed10dd5ba
felipeneneu <feliperpg3@gmail.com>
2025-12-27 08:38:31 -0300

    Building data filters

---
+++ src/features/tasks/components/data-filters.tsx
@@ -0,0 +1,156 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/src/ui/select";
+import { useGetMembers } from "../../members/api/use-get-members";
+import { useGetProjects } from "../../projects/api/use-get-projects";
+import { useWorkspaceId } from "../../workspaces/hooks/use-workspace-id";
+import { FolderIcon, ListCheckIcon, UserIcon } from "lucide-react";
+import { TaskStatus } from "../types";
+import { useTaskFilters } from "../hooks/use-task-filters";
+import { DatePicker } from "@/src/ui/date-picker";
+
+interface DataFiltersProps {
+  hideProjectFilters?: boolean;
+}
+
+export const DataFilters = ({ hideProjectFilters }: DataFiltersProps) => {
+  const workspaceId = useWorkspaceId();
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  const projectOptions = projects?.documents.map((project) => ({
+    value: project.$id,
+    label: project.name,
+  }));
+
+  const memberOptions = members?.documents.map((member) => ({
+    value: member.$id,
+    label: member.name,
+  }));
+
+  const [{ status, assigneeId, projectId, dueDate }, setFilters] =
+    useTaskFilters();
+
+  const onStatusChange = (value: string) => {
+    if (value === "all") {
+      setFilters({ status: null });
+    } else {
+      setFilters({ status: value as TaskStatus });
+    }
+  };
+
+  const onAssigneeChange = (value: string) => {
+    setFilters({ assigneeId: value === "all" ? null : (value as string) });
+  };
+
+  const onProjectChange = (value: string) => {
+    setFilters({ projectId: value === "all" ? null : (value as string) });
+  };
+  if (isLoading) return null;
+  return (
+    <div className="flex flex-col lg:flex-row gap-2">
+      <Select
+        defaultValue={status ?? undefined}
+        onValueChange={(value) => onStatusChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8" size="sm">
+          <div className="flex items-center pr-2">
+            <ListCheckIcon className="size-4 mr-2" />
+            <SelectValue placeholder="All statuses" />
+          </div>
+        </SelectTrigger>
+        <SelectContent
+          side="bottom"
+          align="start"
+          sideOffset={2}
+          position="popper"
+        >
+          <SelectItem value="all">All statuses</SelectItem>
+          <SelectSeparator />
+          <SelectItem value={TaskStatus.BACKLOG}>Backlog</SelectItem>
+          <SelectItem value={TaskStatus.IN_PROGRESS}>In progress</SelectItem>
+          <SelectItem value={TaskStatus.IN_REVIEW}>In review</SelectItem>
+          <SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
+          <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Select
+        defaultValue={assigneeId ?? undefined}
+        onValueChange={(value) => onAssigneeChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8" size="sm">
+          <div className="flex items-center pr-2">
+            <UserIcon className="size-4 mr-2" />
+            <SelectValue
+              placeholder="All assignees"
+              className="placeholder:text-black"
+            />
+          </div>
+        </SelectTrigger>
+        <SelectContent
+          side="bottom"
+          align="start"
+          sideOffset={2}
+          position="popper"
+        >
+          <SelectItem value="all">All assignees</SelectItem>
+          <SelectSeparator />
+          {memberOptions?.map((member) => (
+            <SelectItem key={member.value} value={member.value}>
+              {member.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      <Select
+        defaultValue={projectId ?? undefined}
+        onValueChange={(value) => onProjectChange(value)}
+      >
+        <SelectTrigger className="w-full lg:w-auto h-8" size="sm">
+          <div className="flex items-center pr-2">
+            <FolderIcon className="size-4 mr-2" />
+            <SelectValue
+              placeholder="All projects"
+              className="placeholder:text-black"
+            />
+          </div>
+        </SelectTrigger>
+        <SelectContent
+          side="bottom"
+          align="start"
+          sideOffset={2}
+          position="popper"
+        >
+          <SelectItem value="all">All projects</SelectItem>
+          <SelectSeparator />
+          {projectOptions?.map((project) => (
+            <SelectItem key={project.value} value={project.value}>
+              {project.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <DatePicker
+        placeholder="Due date"
+        className="h-8 w-full lg:w-auto"
+        value={dueDate ? new Date(dueDate) : undefined}
+        onChange={(date) => {
+          setFilters({ dueDate: date ? date.toISOString() : null });
+        }}
+      />
+    </div>
+  );
+};
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,16 +1,42 @@
 "use client";
-import { PlusIcon } from "lucide-react";
+import { Loader, PlusIcon } from "lucide-react";
 
 import { Button } from "@/src/ui/button";
 import { DottedSeparator } from "@/src/ui/dotted-separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/src/ui/tabs";
 
 import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import { useGetTasks } from "../api/use-get-tasks";
+import { useWorkspaceId } from "../../workspaces/hooks/use-workspace-id";
+import { useQueryState } from "nuqs";
+import { DataFilters } from "./data-filters";
+import { useTaskFilters } from "../hooks/use-task-filters";
 
 export const TaskViewSwitcher = () => {
+  const [view, setView] = useQueryState("task-view", {
+    defaultValue: "table",
+  });
+
+  const [{ status, assigneeId, projectId, dueDate }, setFilters] =
+    useTaskFilters();
+
+  const workspaceId = useWorkspaceId();
   const { open } = useCreateTaskModal();
+
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+    projectId,
+    assigneeId,
+    status,
+    dueDate,
+  });
+
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
@@ -30,20 +56,25 @@ export const TaskViewSwitcher = () => {
           </Button>
         </div>
         <DottedSeparator className="my-4" />
-        {/** Add Filters */}
+        <DataFilters />
         <DottedSeparator className="my-4" />
-
-        <>
-          <TabsContent value="table" className="mt-0">
-            Data Table
-          </TabsContent>
-          <TabsContent value="kaban" className="mt-0">
-            Kaban
-          </TabsContent>
-          <TabsContent value="calendar" className="mt-0">
-            Calendar
-          </TabsContent>
-        </>
+        {isLoadingTasks ? (
+          <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+            <TabsContent value="kaban" className="mt-0">
+              Kaban
+            </TabsContent>
+            <TabsContent value="calendar" className="mt-0">
+              Calendar
+            </TabsContent>
+          </>
+        )}
       </div>
     </Tabs>
   );
