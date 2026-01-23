9feef177b820f0c8863ece06acd3d7694b0d3430
Felipe Neneu <164976338+felipeneneu@users.noreply.github.com>
2026-01-23 11:05:46 -0300

    Update OAuthButtons.tsx

--- src/features/auth/components/OAuthButtons.tsx
+++ src/features/auth/components/OAuthButtons.tsx
@@ -1,21 +1,17 @@
 // src/components/OAuthButtons.tsx
-'use server'
+'use client';
+
 import { signUpWithGoogle, signUpWithGithub } from "@/src/lib/oauth";
 
 export function OAuthButtons() {
   return (
     <div className="flex flex-col gap-y-4">
       <form action={signUpWithGoogle}>
-        <Button variant="secondary" size="lg" className="w-full">
-          <FcGoogle className="mr-2 size-5" />
-          Login com Google
-        </Button>
+        <button type="submit">Login com Google</button>
       </form>
+
       <form action={signUpWithGithub}>
-        <Button variant="secondary" size="lg" className="w-full">
-          <FaGithub className="mr-2 size-5" />
-          Login com Github
-        </Button>
+        <button type="submit">Login com Github</button>
       </form>
     </div>
   );
